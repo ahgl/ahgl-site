40b6c66d7cbeff568be0a9a7e04e834d2369f5dc
Ed Ceaser <eac@tehasdf.com>
2012-11-02 19:15:14 -0700

    Fix adding a tournament when running under local task execution mode

--- ahgl/apps/tournaments/admin.py
+++ ahgl/apps/tournaments/admin.py
@@ -12,6 +12,7 @@ from .models import Tournament, TournamentRound, Map, Match, Game, TeamRoundMemb
 from apps.profiles.models import Team, Profile, TeamMembership
 
 from .tasks import update_round_stats
+from local_settings import CELERY_ALWAYS_EAGER
 
 class TournamentRoundInline(admin.TabularInline):
     model = TournamentRound
@@ -46,6 +47,8 @@ class TournamentAdmin(admin.ModelAdmin):
         else:
             transaction.commit()
             update_round_stats.delay(obj.pk)
+            if CELERY_ALWAYS_EAGER is True:
+              transaction.commit() # must commit if running in eager task mode
 
     def get_urls(self):
         urls = super(TournamentAdmin, self).get_urls()
