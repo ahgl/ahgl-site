2eea8d1117bd2f18220e6d5b893681c7273730e9
Nathaniel Tucker <me@ntucker.me>
2013-02-28 15:19:22 -0800

    test

--- ahgl/manage.py
+++ ahgl/manage.py
@@ -8,8 +8,12 @@ sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)),'apps
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ahgl.settings")
 
-    #import ahgl.startup as startup
-    #startup.run()
+    import env
+    # setup the environment for Django and Pinax
+    env.setup_environ(__file__)
+
+    import ahgl.startup as startup
+    startup.run()
 
     from django.core.management import execute_from_command_line
 
