12c2d5ddd977591b357c7f8423c957351bc7f57f
Dimitri Gnidash <dimitri.gnidash@gmail.com>
2014-09-04 15:15:56 -0400

    -Add ability to link through

--- ahgl/api/serializers/match.py
+++ ahgl/api/serializers/match.py
@@ -7,7 +7,8 @@ class MatchSerializer(serializers.HyperlinkedModelSerializer):
     away_team = serializers.SlugRelatedField(slug_field='name')
     tournament = serializers.SlugRelatedField(slug_field='name')
     background_image_url = serializers.CharField(source='get_background_image_url')
+    match_url = serializers.CharField(source='get_absolute_url')
 
     class Meta:
         model = Match
-        fields = ('home_team', 'away_team', 'tournament', 'featured', 'background_image_url')
+        fields = ('home_team', 'away_team', 'tournament', 'featured', 'background_image_url', 'match_url')
