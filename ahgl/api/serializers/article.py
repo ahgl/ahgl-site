8eb978e3a845115c98933650aeb227ff92c11cd9
Ani Tumanyan <ani.voskanyan@gmail.com>
2014-09-04 05:14:17 -0400

    An article serializer

---
+++ ahgl/api/serializers/article.py
@@ -0,0 +1,22 @@
+from ahgl.tournaments.models import Article
+from ahgl.api.models import Game
+from rest_framework import serializers
+
+class ArticleSerializer(serializers.HyperlinkedModelSerializer):
+	tournaments = serializers.SlugRelatedField(many=True, read_only=True, slug_field='name')
+	image_url = serializers.SerializerMethodField('get_image_url')
+	page_url = serializers.CharField(source='get_absolute_url')
+
+	class Meta:
+		model = Article
+		fields = ('title', 'summary', 'tournaments', 'image_url', 'page_url',)
+
+	def get_image_url(self, article):
+		image_url = None
+
+		if hasattr(article, 'tournament'):
+			game_queryset = Game.objects.filter(tournament=article.tournament)
+			if game_queryset.exists():
+				image_url = game_queryset.get().image_url
+
+		return image_url
