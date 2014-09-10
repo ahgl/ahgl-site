78aece1e95d4d81c8e2140a95e53ab1b65b298ac
Ian Langworth ☠ <ian@langworth.com>
2014-09-10 08:39:27 -0700

    Remove all traces of unlicensed fonts (fixes #50)

--- ahgl/static/js/app/Gruntfile.js
+++ ahgl/static/js/app/Gruntfile.js
@@ -223,8 +223,7 @@ module.exports = function (grunt) {
                 src: [
                     '<%= yeoman.dist %>/scripts/{,*/}*.js',
                     '<%= yeoman.dist %>/styles/{,*/}*.css',
-                    '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-                    '<%= yeoman.dist %>/styles/fonts/*'
+                    '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
                 ]
             }
         },
@@ -383,8 +382,7 @@ module.exports = function (grunt) {
                         '.htaccess',
                         '*.html',
                         'views/{,*/}*.html',
-                        'images/{,*/}*.{webp}',
-                        'fonts/*'
+                        'images/{,*/}*.{webp}'
                     ]
                 }, {
                     expand: true,
--- ahgl/static/js/app/app/scripts/config.js
+++ ahgl/static/js/app/app/scripts/config.js
@@ -3,6 +3,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {name:'development',apiEndpoint:'http://127.0.0.1:8000'})
+.constant('ENV', {name:'production',apiEndpoint:''})
 
 ;
