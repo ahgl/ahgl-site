a00b8c465f7d5e036beba98d5ea3a71b2037b83d
Ani Tumanyan <ani.voskanyan@gmail.com>
2014-09-11 08:28:07 -0400

    Add a new task notifications to team captains about new join requests.

--- ahgl/profiles/tasks.py
+++ ahgl/profiles/tasks.py
@@ -1,6 +1,18 @@
 from celery.task import task
 from account.models import AccountDeletion
 
+from django.contrib.auth.models import User
+from notification import models as notification
+
 @task(name="profiles.tasks.expunge_deleted", ignore_result=True)
 def expunge_deleted():
     return AccountDeletion.expunge()
+
+@task(ignore_result=True)
+def notify_member_join_request(team, member, membership_id, captains):
+    notification.send((User.objects.filter(pk__in=captains)),
+                      "profiles_member_join_request",
+                      {
+                        'team': team,
+                        'member': member,
+                        'membership_id': membership_id})
