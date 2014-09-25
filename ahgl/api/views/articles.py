137879f92e61f0470ee04149aa307dbf39c225f9
Ani Tumanyan <ani.voskanyan@gmail.com>
2014-09-25 13:13:56 -0400

    Accepting tournament slug. Getting only active tournaments.

--- ahgl/api/views/articles.py
+++ ahgl/api/views/articles.py
@@ -1,4 +1,5 @@
 from ahgl.tournaments.models import Article
+from ahgl.tournaments.models import Tournament
 from rest_framework import viewsets
 from ahgl.api.serializers import article
 
@@ -8,16 +9,17 @@ class LatestNewsViewSet(viewsets.ModelViewSet):
     serializer_class = article.ArticleSerializer
 
     def get_queryset(self):
-        limit = self.request.QUERY_PARAMS.get('limit', 2)
-        game = self.request.QUERY_PARAMS.get('game', None)
+        limit = int(self.request.QUERY_PARAMS.get('limit', 2))
+        tournament_slug = self.request.QUERY_PARAMS.get('tournament', None)
 
-        if game:
-            queryset = Article.objects.filter(tournaments__game__slug=game, published=True).order_by('publish_date', 'creation_date')[:limit]
+        if tournament_slug:
+            tournament = Tournament.objects.filter(slug=tournament_slug, status='A')
+            queryset = Article.objects.filter(tournaments=tournament, published=True).order_by('publish_date', 'creation_date')[:limit]
 
             for article in queryset:
-                article.tournament = game
+                article.tournaments = tournament
                     
         else:
-            queryset = Article.objects.filter(published=True).order_by('publish_date', 'creation_date')[:limit]
+            queryset = Article.objects.filter(tournaments__status='A', published=True).distinct().order_by('publish_date', 'creation_date')[:limit]
 
         return queryset
