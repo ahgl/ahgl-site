571521e8a23898964b5b605b08f34516c1594d6e
Dimitri Gnidash <dimitri.gnidash@gmail.com>
2014-09-28 12:54:21 -0400

    -Uncomment migration files -Remove the key

--- ahgl/tournaments/migrations/0051_auto__add_field_tournament_game.py
+++ ahgl/tournaments/migrations/0051_auto__add_field_tournament_game.py
@@ -16,9 +16,8 @@ class Migration(SchemaMigration):
 
     def backwards(self, orm):
         # Deleting field 'Tournament.game'
-        #db.delete_column('tournaments_tournament', 'game_id')
-        ""
-
+        db.delete_column('tournaments_tournament', 'game_id')
+        
     models = {
         'api.game': {
             'Meta': {'object_name': 'Game'},
