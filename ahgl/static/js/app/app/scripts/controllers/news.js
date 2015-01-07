e0d630e856bbd0d8d2c861f4dc6df8fa79f3cd35
Ani Tumanyan <ani.voskanyan@gmail.com>
2015-01-07 10:55:10 -0500

    getMonth method returns an integer between 0 and 11. So there was no need to substract 1 from the getMonth() number.

--- ahgl/static/js/app/app/scripts/controllers/news.js
+++ ahgl/static/js/app/app/scripts/controllers/news.js
@@ -22,7 +22,7 @@ angular.module('ahglApp')
             if (!date) {
                 return "";
             }
-            var dateStr = monthNames[date.getMonth() - 1] + " " + date.getDay() + ", " + date.getFullYear()
+            var dateStr = monthNames[date.getMonth()] + " " + date.getDay() + ", " + date.getFullYear()
             return dateStr;
         };
 
